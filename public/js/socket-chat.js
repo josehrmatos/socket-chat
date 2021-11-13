bab27d1da5dd190bb810821694bb0d42dafe7422
Jose Hilario Reyes Matos <josreyes@apap.com.do>
2021-11-12 23:46:19 -0400

    chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,23 +2,26 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
-
 
 var usuario = {
     nombre: params.get('nombre'),
-    sala:params.get('sala')
+    sala: params.get('sala')
 };
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat',usuario,function(resp){
-        console.log('Usuarios conectados ',resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -31,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -39,21 +42,19 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-
-//listaPersona
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log( personas);
-
+    renderizarUsuarios(personas);
 });
 
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-socket.on('mensajePrivado',function(mensaje){
-    console.log('Mensaje privado:',mensaje);
-})
+    console.log('Mensaje Privado:', mensaje);
+
+});
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -20,19 +20,21 @@ io.on('connection', (client) => {
 
         let personas = usuarios.agregarPersona(client.id,data.nombre,data.sala);
         client.broadcast.to(data.sala).emit('listaPersona',usuarios.getPersonasPorSala(data.sala));
-        
+        client.broadcast.to(data.sala).emit('crearMensaje',crearMensaje('Administrador',`${data.nombre} se unio`));
         callback(personas);
     });
 
 
 
-    client.on('crearMensaje',(data)=>{
+    client.on('crearMensaje',(data,callback)=>{
 
 
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
 
         client.broadcast.to(persona.sala).emit('crearMensaje',mensaje);
+        
+        callback(mensaje);
     });
 
 
